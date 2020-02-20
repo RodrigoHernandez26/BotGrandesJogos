3b858bf56da473e12f4ba9f8d275f05d670710bf
RodrigoHernandez26 <rodrigojipe@hotmail.com>
2020-02-19 23:20:00 -0300

    Adicionado o comando ?help Adicionado a possibilidade de adicionar/retirar mais de 1 ponto em um unico comando

--- main.py
+++ main.py
@@ -18,12 +18,10 @@ def log():
     log_nomes = open('log_nomes.txt', 'r')
     for i in log_nomes.readline().split():
         lista_nomes.append(i)
-    print(lista_nomes)
     log_nomes.close()
     log_pontos = open('log_pontos.txt', 'r')
     for i in log_pontos.readline().split():
         lista_pontos.append(i)
-    print(lista_pontos)
     log_pontos.close()
 
 def capitalizacao(nome):
@@ -80,14 +78,15 @@ async def on_message(message):
 
     if message.content.lower().startswith('?novo'):
         nome = capitalizacao(message.content[6:])
-        print(nome)
         if nome in lista_nomes:
             novo_embedt = discord.Embed(
                 title = 'O nome ' + nome + ' já foi adicionado ao jogo.',
                 color = 0x22a7f0
             )
+            novo_embedt.set_footer(text = '?help para ajuda')
             await message.channel.send(embed = novo_embedt)
             write_log(hora() + ' - O ' + message.author.name + ' tentou adicionar o ' + nome + ' novamente ao jogo.')
+            print(hora() + ' - O ' + message.author.name + ' tentou adicionar o ' + nome + ' novamente ao jogo.')
         else:
             lista_nomes.append(nome)
             lista_pontos.append('1')
@@ -96,24 +95,32 @@ async def on_message(message):
                 title = 'O nome ' + nome + ' foi adicionado ao jogo!',
                 color = 0x22a7f0
             )
+            novo_embedf.set_footer(text = '?help para ajuda')
             await message.channel.send(embed = novo_embedf)
             write_log(hora() + ' - O ' + message.author.name + ' adicionou o ' + nome + ' ao jogo.')
-            print(lista_nomes)
-                
+            print(hora() + ' - O ' + message.author.name + ' adicionou o ' + nome + ' ao jogo.')
+                 
     if message.content.lower().startswith('?pontos'):
-        print(lista_pontos)
         embed = discord.Embed(
             title = 'Os pontos são: ',
             color = 0x22a7f0
         )
+        embed.set_footer(text = '?help para ajuda')
         for i in range(len(lista_nomes)):
-            embed.add_field(name = lista_nomes[i], value = lista_pontos[i], inline= True)
+            if i == 0:
+                embed.add_field(name = lista_nomes[i] + '🥇', value = lista_pontos[i], inline= True)
+            elif i == 1:
+                embed.add_field(name = lista_nomes[i] + '🥈', value = lista_pontos[i], inline= True)
+            elif i == 2:
+                embed.add_field(name = lista_nomes[i] + '🥉', value = lista_pontos[i], inline= True)
+            else:
+                embed.add_field(name = lista_nomes[i], value = lista_pontos[i], inline= True)
         await message.channel.send(embed = embed)
         write_log(hora() + ' - O ' + message.author.name + ' solicitou a listagem dos pontos dos participantes do jogo.')
+        print(hora() + ' - O ' + message.author.name + ' solicitou a listagem dos pontos dos participantes do jogo.')
 
     if message.content.lower().startswith('?remover'):
         nome = capitalizacao(message.content[9:])
-        print(nome)
         pos = lista_nomes.index(nome)
         lista_nomes.remove(nome)
         del(lista_pontos[pos])
@@ -122,37 +129,76 @@ async def on_message(message):
             title = 'O nome ' + nome + ' foi retirado do jogo!',
             color = 0x22a7f0
         )
+        revomer_embed.set_footer(text = '?help para ajuda')
         await message.channel.send(embed = revomer_embed)
         write_log(hora() + ' - O ' + message.author.name + ' retirou o ' + nome + ' do jogo.')
+        print(hora() + ' - O ' + message.author.name + ' retirou o ' + nome + ' do jogo.')
 
     if message.content.lower().startswith('?add'):
-        nome = capitalizacao(message.content[5:])
-        print(nome)
+        numero = message.content[5:6]
+        nome = capitalizacao(message.content[7:])
         pos = lista_nomes.index(nome)
         conc = int(lista_pontos[pos])
-        print(conc)
-        lista_pontos[pos] = conc + 1
+        lista_pontos[pos] = conc + int(numero)
         organizar(lista_nomes, lista_pontos)
-        add_embed = discord.Embed(
-            title = 'For adicionado 1 ponto ao ' + nome + '!',
+        add_embeds = discord.Embed(
+            title = 'Foi adicionado 1 ponto ao ' + nome + '!',
             color = 0x22a7f0
         )
-        await message.channel.send(embed = add_embed)
-        write_log(hora() + ' - O ' + message.author.name + ' adicionou 1 ponto ao ' + nome + ', ele tem ' + str(lista_pontos[pos]) + ' agora.')
+        add_embedp = discord.Embed(
+            title = 'Foi adicionado ' + numero + ' pontos ao ' + nome + '!',
+            color = 0x22a7f0
+        )
+        add_embedp.set_footer(text = '?help para ajuda')
+        add_embeds.set_footer(text = '?help para ajuda')
+        if numero == '1':
+            await message.channel.send(embed = add_embeds)
+            write_log(hora() + ' - O ' + message.author.name + ' adicionou 1 ponto ao ' + nome + ', ele tem ' + str(lista_pontos[pos]) + ' agora.')
+            print(hora() + ' - O ' + message.author.name + ' adicionou 1 ponto ao ' + nome + ', ele tem ' + str(lista_pontos[pos]) + ' agora.')
+        else:
+            await message.channel.send(embed = add_embedp)
+            write_log(hora() + ' - O ' + message.author.name + ' adicionou ' + numero + ' pontos ao ' + nome + ', ele tem ' + str(lista_pontos[pos]) + ' agora.')
+            print(hora() + ' - O ' + message.author.name + ' adicionou ' + numero + ' pontos ao ' + nome + ', ele tem ' + str(lista_pontos[pos]) + ' agora.')
 
     if message.content.lower().startswith('?retirar'):
-        nome = capitalizacao(message.content[9:])
-        print(nome)
+        numero = message.content[9:10]
+        nome = capitalizacao(message.content[11:])
         pos = lista_nomes.index(nome)
         conc = int(lista_pontos[pos])
-        print(conc)
-        lista_pontos[pos] = conc - 1
+        lista_pontos[pos] = conc - int(numero)
         organizar(lista_nomes, lista_pontos)
-        retirar_embed = discord.Embed(
+        retirar_embeds = discord.Embed(
             title = 'Foi retirado 1 ponto do ' + nome + '!',
             color = 0x22a7f0
         )
-        await message.channel.send(embed = retirar_embed)
-        write_log(hora() + ' - O ' + message.author.name + ' retirou 1 ponto ao ' + nome + ', ele tem ' + str(lista_pontos[pos]) + ' agora.')
+        retirar_embedp = discord.Embed(
+            title = 'Foi retirado ' + numero + ' pontos do ' + nome + '!',
+            color = 0x22a7f0
+        )
+        retirar_embeds.set_footer(text = '?help para ajuda')
+        retirar_embedp.set_footer(text = '?help para ajuda')
+        if numero == '1':
+            await message.channel.send(embed = retirar_embeds)
+            write_log(hora() + ' - O ' + message.author.name + ' retirou 1 ponto ao ' + nome + ', ele tem ' + str(lista_pontos[pos]) + ' agora.')
+            print(hora() + ' - O ' + message.author.name + ' retirou 1 ponto ao ' + nome + ', ele tem ' + str(lista_pontos[pos]) + ' agora.')
+        else:
+            await message.channel.send(embed = retirar_embedp)
+            write_log(hora() + ' - O ' + message.author.name + ' retirou ' + numero + ' pontos ao ' + nome + ', ele tem ' + str(lista_pontos[pos]) + ' agora.')
+            print(hora() + ' - O ' + message.author.name + ' retirou ' + numero + ' pontos ao ' + nome + ', ele tem ' + str(lista_pontos[pos]) + ' agora.')
 
+    if message.content.lower().startswith('?help'):
+        help_embed = discord.Embed(
+            title = 'Como usar todos os comandos: ',
+            color = 0x22a7f0
+        )
+        help_embed.set_footer(text = 'Verifique como está escrito o nome da pessoa pelo comando ?pontos. Os comandos não tem sensibilidade a capitalização.')
+        help_embed.add_field(name = '?novo', value = 'Adiciona uma nova pessoa ao jogo. Ex: ?novo NomeDaPessoa', inline = False)
+        help_embed.add_field(name = '?pontos', value = 'Lista os pontos por participante. Ex: ?pontos', inline = False)
+        help_embed.add_field(name = '?remover', value = 'Remove uma pessoa do jogo. Ex: ?remover NomeDaPessoa', inline = False)
+        help_embed.add_field(name = '?add', value = 'Adiciona pontos a uma pessoa. Ex: ?add 1 NomeDaPessoa (no máx 9)', inline = False)
+        help_embed.add_field(name = '?retirar', value = 'Retira pontos de uma pessoa. Ex: ?retirar 1 NomeDaPessoa (no máx 9)', inline = False)
+        await message.channel.send(embed = help_embed)
+        write_log(hora() + ' - O ' + message.author.name + ' usou o ?help.')
+        print(hora() + ' - O ' + message.author.name + ' usou o ?help.')
+        
 bot.run('Njc5MTUzNzU0MTc1NzAxMDMy.XktNSw.nY9_kDFHRlP3Bp36NT0X_CJ2J7Y')
