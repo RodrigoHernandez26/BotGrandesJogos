8830d7865906d3a6728d65e26a1bc2111accfb76
Rodrigo <rodrigojipe@hotmail.com>
2020-03-25 03:46:15 -0300

    Removido erro do log no var

--- cogs/var.py
+++ cogs/var.py
@@ -15,21 +15,22 @@ class Var(commands.Cog):
 
         with open('data.json', 'r') as f: data = json.load(f)
         self.data = data
-
+        
     @commands.command()
     async def var(self, ctx, ponto, nome, *, msg):
+
+        canal_log = self.client.get_channel(self.data['log'])
+        self.canal_log = canal_log
 
         self.nome = capitalizacao(nome)
         self.ponto = int(ponto)
         self.msg = msg
         self.autor = ctx.author.name
 
-        canal_log = self.client.get_channel(self.data['log'])
-
         if not self.var_on:
 
             self.msg_bot = await ctx.send(embed = criar_var(msg, self.autor, self.nome, self.ponto))
-            await canal_log.send(f'{ctx.author.name} criou um var.')
+            await self.canal_log.send(f'{ctx.author.name} criou um var.')
 
             await self.msg_bot.add_reaction('✅')
             await self.msg_bot.add_reaction('❌')
@@ -38,12 +39,12 @@ class Var(commands.Cog):
 
         else:
             await ctx.send(embed = var_fail())
-            await canal_log.send(f'{self.autor} tentou criar um novo var, mas já existe uma votação em andamento.')
+            await self.canal_log.send(f'{self.autor} tentou criar um novo var, mas já existe uma votação em andamento.')
 
     @commands.command()
     async def cancelarvar(self, ctx):
 
-        canal_log = self.client.get_channel(self.data['log'])
+        self.canal_log = self.client.get_channel(self.data['log'])
 
         if self.var_on:
             if ctx.author.name == self.autor:
@@ -60,18 +61,18 @@ class Var(commands.Cog):
                 with open('data.json', 'w') as f: json.dump(self.data, f, indent= 4)
 
                 await ctx.send(embed = var_cancelado())
-                await canal_log.send(f'{ctx.author.name} cancelou o var.')
+                await self.canal_log.send(f'{ctx.author.name} cancelou o var.')
             
             else:
                 ctx.send(embed = var_autor())
-                await canal_log.send(f'{ctx.author.name} tentou cancelar uma var que não era seu.')
+                await self.canal_log.send(f'{ctx.author.name} tentou cancelar uma var que não era seu.')
 
         else:
             await ctx.send(embed = var_erro())
-            await canal_log.send(f'{ctx.author.name} tentou cancelar um var que não existe.')
+            await self.canal_log.send(f'{ctx.author.name} tentou cancelar um var que não existe.')
 
     @commands.command()
-    async def cheat(self, ctx):   
+    async def cheat(self, ctx):
 
         if self.cheat_mode:
             self.cheat_mode = False
@@ -84,8 +85,6 @@ class Var(commands.Cog):
         self.num_n = 0
         erro = False
 
-        canal_log = self.client.get_channel(self.data['log'])
-
         for name in self.data['var']:
             if name['nome'] == user.name:
                 erro = True
@@ -96,7 +95,7 @@ class Var(commands.Cog):
                 with open('data.json', 'w') as f: json.dump(self.data, f, indent= 4)
 
                 await self.msg_bot.edit(embed = criar_var(self.msg, self.autor, self.nome, self.ponto))
-                await canal_log.send(f'{user.name} votou {reaction.emoji} no var.')
+                await self.canal_log.send(f'{user.name} votou {reaction.emoji} no var.')
                 self.num_p = reaction.count - 1
 
             elif reaction.emoji == '\u274c':
@@ -105,7 +104,7 @@ class Var(commands.Cog):
                 with open('data.json', 'w') as f: json.dump(self.data, f, indent= 4)
 
                 await self.msg_bot.edit(embed = criar_var(self.msg, self.autor, self.nome, self.ponto))
-                await canal_log.send(f'{user.name} votou {reaction.emoji} no var.')
+                await self.canal_log.send(f'{user.name} votou {reaction.emoji} no var.')
                 self.num_n = reaction.count - 1
 
         if (self.num_n == 5 or self.num_p == 5) or self.cheat_mode:
@@ -129,7 +128,7 @@ class Var(commands.Cog):
                 resultado = 'Xitado!'
 
             await self.msg_bot.channel.send(embed = var_final(self.msg, self.autor, resultado, self.nome, self.ponto))
-            await canal_log.send(f'A votação foi encerrada, o resultado foi: {resultado}')
+            await self.canal_log.send(f'A votação foi encerrada, o resultado foi: {resultado}')
 
             self.var_on = False
 
@@ -144,8 +143,6 @@ class Var(commands.Cog):
 
     @commands.Cog.listener()
     async def on_reaction_remove(self, reaction, user):
-
-        canal_log = self.client.get_channel(self.data['log'])
         
         for name in self.data['var']:
             if user.name == name['nome']:
@@ -159,7 +156,7 @@ class Var(commands.Cog):
                 if name['nome'] == user.name:
                     self.data['var'].pop(cont - 1)
 
-                    await canal_log.send(f'{user.name} retirou seu voto: {reacao}')
+                    await self.canal_log.send(f'{user.name} retirou seu voto: {reacao}')
 
                     with open('data.json', 'w') as f: json.dump(self.data, f, indent= 4)
                     
